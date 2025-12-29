440f3bd98f6e4cc88dd356cfcd5f6525ea521f68
chrispaganon <info@chrispaganon.com>
2025-12-29 10:13:00 +0100

    add edit & upload to upload script

--- .config/sway/screenshot-upload.sh
+++ .config/sway/screenshot-upload.sh
@@ -4,6 +4,10 @@ now=$(date +%Y-%m-%dT%H:%M:%S%Z)
 filename_base="$HOME/Pictures/screenshots/$now"
 filename="$filename_base.jpg"
 
-grimshot save area "$filename"
+if [[ "$1" == "--edit" || "$1" == "-e"  ]]; then
+    grim -g "$(slurp)" - | swappy -f - -o "$filename"
+else
+    grimshot save area "$filename"
+fi
 
 curl -i -F"file=@$filename" https://0x0.st | tee "$filename_base.txt" | tail --lines 1 | wl-copy
